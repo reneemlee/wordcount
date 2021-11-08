3e6dac3288b7ead990fd674f4f16c5ed3478bf57
Renee <reneemlee92@gmail.com>
2021-11-08 20:57:17 +0000

    complete further study

--- wordcount.py
+++ wordcount.py
@@ -1,17 +1,47 @@
 """Count words in file."""
-#function, take in filename
+import sys
+import re
+
+
 def word_count(filename):
+    tokens = tokenize(filename)
+    dict_of_words = count_words(tokens)
+    print_word_counts(dict_of_words)
+
+    # with open(filename) as file:
+
+    #     word_count_dict = {}
+
+    #     for line in file:       
+    #         line = line.rstrip().split()
+
+    #         for word in line:
+    #             word_count_dict[word] = word_count_dict.get(word, 0) + 1
+
+    #     return word_count_dict
+
+
+def tokenize(filename):
+    words = []
     with open(filename) as file:
-
-        word_count_dict = {}
-
-        for line in file:       
+        for line in file:
             line = line.rstrip().split()
+            words.extend(line)
+    return words
 
-            for word in line:
-                word_count_dict[word] = word_count_dict.get(word, 0) + 1
+def count_words(words):
+    word_count_dict = {} 
+    for word in words:
+        word = word.lower()
+        #removing spaces and punctuations from a string
+        word = re.sub(r'[^\w\s]','',word)
+        word_count_dict[word] = word_count_dict.get(word, 0) + 1
+    
+    return word_count_dict
 
-        return word_count_dict
+def print_word_counts(word_counts):
+    for key, value in word_counts.items():
+        print(f"{key} {value}")
 
-
-print(word_count("test.txt"))
+# print_word_counts({'apple': 2, 'berry': 1, 'cherry': 1})
+word_count(sys.argv[1])
